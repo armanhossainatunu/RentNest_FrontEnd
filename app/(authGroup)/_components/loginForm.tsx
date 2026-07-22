d8bcb77758c663e8b85e3fb3c998b136b7e75e1e
Arman Hossain Atunu <armanhossainatunu@gmail.com>
2026-07-22 18:12:37 +0600

    some work login page

---
+++ app/(authGroup)/_components/loginForm.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import { Card } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+
+const loginForm = () => {
+  return (
+   <form>
+    <Card>
+    <Input/>
+    <Input/>
+    </Card>
+   </form>
+  )
+}
+
+export default loginForm
---
+++ app/(authGroup)/login/page.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const loginPage = () => {
+  return (
+    <>
+      <div className="flex min-h-screen items-center justify-center">
+        <div className="flex flex-col items-center justify-center gap-4">
+          <h1 className="text-3xl font-bold">Wellcome to Back!</h1>
+          <p className="text-muted-foreground">
+            Enter your credentials to access your account
+          </p>
+        </div>
+
+        
+      </div>
+    </>
+  );
+};
+
+export default loginPage;
