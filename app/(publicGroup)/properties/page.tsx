53786956b963421619e44707c6a954c0476a6332
Arman Hossain Atunu <armanhossainatunu@gmail.com>
2026-07-29 02:21:49 +0600

    properties get just show item wait

--- app/(publicGroup)/properties/page.tsx
+++ app/(publicGroup)/properties/page.tsx
@@ -1,12 +1,25 @@
-import React from 'react'
+async function getProperties() {
+  const res = await fetch(
+    "https://rent-nest-ten.vercel.app/api/properties",
+    {
+      cache: "no-store",
+    }
+  );
 
-const Publicpage = () => {
-  
-  return (
-    <div>
-      <h1>Public page</h1>
-    </div>
-  )
+  return res.json();
 }
 
-export default Publicpage
+export default async function Properties() {
+  const properties = await getProperties();
+  console.log(properties," properties this is");
+
+  return (
+    <div>
+      {properties.data?.map((property: any) => (
+        <div key={property.id}>
+          <h2>{property.title}</h2>
+        </div>
+      ))}
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { Navbar } from "@/components/shared/navbar";
 import { getMe } from "@/service/getMe";
+import Properties from "./(publicGroup)/properties/page";
 
 
 export default async function Home() {
@@ -8,6 +9,7 @@ export default async function Home() {
   return (
    <div className="text-center font-bold ">
     <Navbar user={user} />
+    <Properties/>
     <h1 className="text-3xl font-bold flex items-center justify-center h-screen">Hello, Next.js world</h1>
    
    </div>
